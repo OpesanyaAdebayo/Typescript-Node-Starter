01a60a2f5916f926979f460143ffcb329d31e453
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-10 14:31:42 +0100

    added home route

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import bodyParser from "body-parser";
+import path from 'path';
+import * as homeController from './controllers/home'
+
+// Create Express server
+const app:express.Express = express();
+
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+
+
+app.set("port", process.env.PORT || 6000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', homeController.index)
+
+
+//Catch 404 and send to error handler
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   let err = new Error('Not Found');
+//   next(err);
+// });
+
+// error handler
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+//   res.locals.name = err.name
+//   res.locals.message = err.message;
+//   res.locals.stack = err.stack;
+//   res.render('error');
+// });
+
+export default app;
