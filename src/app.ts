30bf4af4e944962a4025246db722cacde2440319
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-10 17:48:01 +0100

    refactored src folder

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import bodyParser from "body-parser";
 import path from 'path';
 import * as homeController from './controllers/home'
-
+// import helmet from 'helmet';
 // Create Express server
 const app:express.Express = express();
 
@@ -14,7 +14,11 @@ app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 4000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+// app.use(helmet({
+//   frameguard: {
+//     action: 'deny'
+//   }
+// }));
 
 app.get('/', homeController.index)
 
