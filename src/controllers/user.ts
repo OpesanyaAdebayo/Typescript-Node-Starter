ccbe143379b61901f8de0da5b5b273f1e2964e0d
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-16 18:08:14 +0100

    added function to check db before creating user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,47 +1,28 @@
-import { Request, Response, NextFunction } from "express";
-import { validationResult } from 'express-validator/check';
+import {
+    Request,
+    Response,
+    NextFunction
+} from "express";
+import {
+    validationResult
+} from 'express-validator/check';
 import User from '../models/User';
 
 export let getLogin = (req: Request, res: Response) => {
-    let { userID } = req.session!;
-    if(userID) {
+    let {
+        userID
+    } = req.session!;
+    if (userID) {
         res.redirect("/");
     }
     res.render("login", {
         title: "Login"
     });
 }
-// export let postLogin = (req: Request, res: Response, next: NextFunction) => {
 
-export let postSignup = (req: Request, res: Response, next: NextFunction) => {
-
-    const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(302).json({errors: errors.array()});
-  }
-
-  const user = new User ({
-      email: req.body.email,
-      password: req.body.password
-  });
-
-  user.save()
-  .then(savedUser => {
-      req.session!.userID = savedUser._id;
-      return res.redirect('/');
-  })
-  .catch(err => {
-    if (err) return next(err);
-
-  })
-
-}
-
-// }
 export let getSignup = (req: Request, res: Response) => {
     let { userID } = req.session!;
-    if(userID) {
+    if (userID) {
         res.redirect("/");
     }
     res.render("signup", {
@@ -49,3 +30,35 @@ export let getSignup = (req: Request, res: Response) => {
     });
 }
 
+export let postSignup = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(302).json({
+      errors: errors.array()
+    });
+  }
+
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  User.findOne({ email: req.body.email }, (err, existingUser) => {
+    if (err) return next(err);
+    if (existingUser) {
+      res
+        .status(302)
+        .json({ error: "account with that email already exists." });
+    }
+    user
+      .save()
+      .then(savedUser => {
+        req.session!.userID = savedUser._id;
+        return res.redirect("/");
+      })
+      .catch(err => {
+        if (err) return next(err);
+      });
+  });
+};
