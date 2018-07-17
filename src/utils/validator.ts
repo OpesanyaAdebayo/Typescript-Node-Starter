2bdcd7e50778f4ca13364631bd4b1a609d9c2f31
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-17 23:41:29 +0100

    used correct syntax

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,6 +1,6 @@
-import { check, validationResult } from 'express-validator/check'
+import { body } from 'express-validator/check'
 
 export const checkSignup = [
-    check("email").isEmail().withMessage("Please enter a valid email address."),
-    check('password').isLength({ min: 7 }).withMessage("Passowrd should be at least 7 characters.")
+    body("email").isEmail().withMessage("Please enter a valid email address."),
+    body("password").isLength({ min: 7 }).withMessage("Password should be at least 7 characters.")
 ]
