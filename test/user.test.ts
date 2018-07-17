ba837bd3038307e3a6a1c45eb487683be171fac7
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-17 10:21:31 +0100

    added test suites for post to login route

--- test/user.test.ts
+++ test/user.test.ts
@@ -8,6 +8,41 @@ describe("GET /login", () => {
   });
 });
 
+describe("POST /login", () => {
+  it("should return error when password is less than 7 characters.", (done) => {
+    return request(app).post("/signup")
+      .field("email", "")
+      .field("password", "vbrmts")
+      .end(function (err, res) {
+        expect(res.error).not.toBe(true);
+        done();
+      })
+      .expect(302)
+  });
+  it("should return error when email is invalid.", (done) => {
+    return request(app).post("/signup")
+      .field("email", "bayytfg")
+      .field("password", "vbrijims")
+      .end(function (err, res) {
+        expect(res.error).not.toBe(true);
+        done();
+      })
+      .expect(302)
+  });
+  it("should return some defined error message with valid parameters", (done) => {
+    return request(app).post("/login")
+      .field("email", "john@me.com")
+      .field("password", "Hunter2")
+      .expect(302)
+      .end(function (err, res) {
+        expect(res.error).not.toBe(undefined)
+        done();
+      });
+
+  });
+});
+
+
 describe("GET /signup", () => {
   it("should return 200 OK", (done) => {
     return request(app).get("/signup")
@@ -17,7 +52,7 @@ describe("GET /signup", () => {
 
 
 describe("POST /signup", () => {
-  it("should return error when passowrd is less than 7 characters.", (done) => {
+  it("should return error when password is less than 7 characters.", (done) => {
     return request(app).post("/signup")
     .field("email", "")
     .field("password", "vbrmts")
@@ -39,3 +74,4 @@ describe("POST /signup", () => {
     .expect(302)
   });
 })
+
