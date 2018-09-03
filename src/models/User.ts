4a7bf292a204b1d6377eda37b8bdbc1a1b507b2d
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-09-03 23:30:49 +0100

    proper pre-save hook syntax

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { HookNextFunction } from 'mongoose';
+import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 export type userModel = mongoose.Document & {
     email: string,
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     password: { type: String },
 });
 
-userSchema.pre("save", function save(this: userModel, next: HookNextFunction) {
+userSchema.pre("save", function save(next) {
     const user = this;
     if (!user.isModified("password")) { return next(); }
     bcrypt.genSalt(10, (err, salt) => {
