f5b4f921d332fc9596ed9fdc8951c817269ff4a6
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-09-03 23:30:17 +0100

    remove unused code

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import app from "./app";
  */
 const server = app.listen(app.get("port"), () => {
   console.log(
-    "  App is running at http://localhost:%d in %s mode",
+    "  App is running at http://localhost:%d",
     app.get("port")
   );
   console.log("  Press CTRL-C to stop\n");
