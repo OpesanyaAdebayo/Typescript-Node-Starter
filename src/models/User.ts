b760fba46572d603d715ac84b5be5a0c090002df
Adebayo Opesanya <onejsninja@Adebayos-MacBook-Pro-2.local>
2018-10-30 13:37:32 +0100

    fix model type error

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,41 +1,58 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose, { HookNextFunction } from "mongoose";
+import bcrypt from "bcrypt";
 export type userModel = mongoose.Document & {
-    email: string,
-    password: string,
-    comparePassword: comparePasswordFunction
-}
+  email: string;
+  password: string;
+  comparePassword: comparePasswordFunction;
+};
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, unique: true },
-    password: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String }
 });
 
-userSchema.pre("save", function save(next) {
-    const user = this;
-    if (!user.isModified("password")) { return next(); }
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) { return next(err); }
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if (err) { return next(err); }
-            user.password = hash;
-            next();
-        })
-
-    })
-});
-
-type comparePasswordFunction = (candidatePassword: string, cb: (err: any, isMatch: any) => {}) => void;
-
-const comparePassword: comparePasswordFunction = function (this:any, candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, (err: mongoose.Error, isMatch: boolean) => {
-        cb(err, isMatch);
+userSchema.pre("save", function save(next: HookNextFunction) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    // @ts-ignore
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      // @ts-ignore
+      user.password = hash;
+      next();
     });
+  });
+});
+
+type comparePasswordFunction = (
+  candidatePassword: string,
+  cb: (err: any, isMatch: any) => {}
+) => void;
+
+const comparePassword: comparePasswordFunction = function(
+  this: any,
+  candidatePassword,
+  cb
+) {
+  bcrypt.compare(
+    candidatePassword,
+    this.password,
+    (err: mongoose.Error, isMatch: boolean) => {
+      cb(err, isMatch);
+    }
+  );
 };
 
 userSchema.methods.comparePassword = comparePassword;
 
 const User = mongoose.model("User", userSchema);
 
-
-export default User
+export default User;
