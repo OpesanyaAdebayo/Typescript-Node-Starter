c825278bda8af3387e44ebe34b1aa2ced795f7a9
Adebayo Opesanya <bayoopesanya@gmail.com>
2020-10-25 17:17:32 +0100

    refactor: change secrets loading pattern

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -1,26 +1,15 @@
 import logger from './logger';
-import fs from 'fs';
 import dotenv from 'dotenv';
-import DotenvResult from 'dotenv';
-const env = dotenv.config();
-dotenv.load();
 
-// if (env.error) {
-//   logger.error("Cannot find a .env file.");
-//   process.exit(1)
-// }
+dotenv.config();
 
-const parsed = env.parsed! || process.env;
-export const MLAB_URI = parsed["MLAB_URI"] || process.env.MLAB_URI!
-export const SESSION_SECRET = parsed["SESSION_SECRET"] || process.env.SESSION_SECRET!;
-
-
-if (!SESSION_SECRET) {
-  logger.error("No client secret. Set SESSION_SECRET environment variable.");
-  process.exit(1);
+function throwIfUndefined(secret: string) {
+  if (!process.env[secret]) {
+    logger.error(`Please set ${secret} environment variable`);
+    process.exit(1);
+  }
+  return secret;
 }
 
-if (!MLAB_URI) {
-  logger.error("No mongo connection string. Set MLAB_URI environment variable.");
-  process.exit(1);
-}
+export const DATABASE_URL = throwIfUndefined('DATABASE_URL');
+export const SECRET_KEY = throwIfUndefined('SECRET_KEY');
