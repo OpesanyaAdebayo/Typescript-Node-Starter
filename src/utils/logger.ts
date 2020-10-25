dd20e038f2b5cfd7092472eaf88209078605f3ab
Adebayo Opesanya <bayoopesanya@gmail.com>
2020-10-25 17:19:00 +0100

    refactor: refactor logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,12 @@
 import winston from 'winston';
-import fs from 'fs';
-import {
-    MLAB_URI,
-    SESSION_SECRET
-} from './secrets';
 
 const logger = winston.createLogger({
-    transports: [
-        new(winston.transports.Console)({
-            level: "debug"
-        }),
-        new(winston.transports.File)({
-            filename: "debug.log",
-            level: "debug"
-        })
-    ]
+  level: 'debug',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'logs/debug.log', level: 'error', silent: true }),
+    new winston.transports.File({ filename: 'logs/combined.log', silent: true }),
+  ],
 });
-
-if (fs.existsSync(".env")) {
-    logger.debug("Using .env file to supply config environment variables");
-}
 
 export default logger;
