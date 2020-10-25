8e76e2456b38211b0ad09dae51796a36861ceebb
Adebayo Opesanya <bayoopesanya@gmail.com>
2020-10-25 17:18:39 +0100

    chore: run eslint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
-import app from "./app";
+import app from './app';
 
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d",
-    app.get("port")
-  );
-  console.log("  Press CTRL-C to stop\n");
+const server = app.listen(app.get('port'), () => {
+  console.log('  App is running at http://localhost:%d', app.get('port'));
+  console.log('  Press CTRL-C to stop\n');
 });
 
 export default server;
